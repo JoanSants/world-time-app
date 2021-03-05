2aa456c5b44cf8f5245e5154140d0de67854ae5f
JoanSantsValid <jsantos.5a@ext.valid.com>
2021-03-05 10:24:43 -0300

    feat: Handle app routes

--- lib/pages/select_location.dart
+++ lib/pages/select_location.dart
@@ -9,6 +9,13 @@ class _SelectLocationState extends State<SelectLocation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        centerTitle: true,
+        elevation: 0,
+        title: Text('Choose location'),
+      ),
+      backgroundColor: Colors.grey[200],
       body: Text('choose location screen'),
     );
   }
