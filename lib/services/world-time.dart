8201962dd9672fcbe3c6e872b5570c9fe3efce98
JoanSantsValid <jsantos.5a@ext.valid.com>
2021-03-10 16:22:56 -0300

    feat: Add worldtime service

---
+++ lib/services/world-time.dart
@@ -0,0 +1,23 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class WorldTime {
+  String dateTime;
+  String location;
+  String flag;
+  String url;
+  
+  WorldTime({ this.dateTime, this.location, this.flag, this.url });
+
+  Future<void> fetchWorldTime () async {
+    http.Response response = await http.get(Uri.https('worldtimeapi.org', '/api/timezone/$url'));
+    Map<String, dynamic> json = jsonDecode(response.body);
+
+    int offset = int.parse((json['utc_offset'] as String).substring(0,3));
+    DateTime now = DateTime.parse(json['datetime']);
+
+    now = offset > 0 ? now.add(Duration(hours: offset)) : now.subtract(Duration(hours: offset.abs()));
+
+    dateTime = now.toString();
+  }
+}
